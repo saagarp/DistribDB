de154ece802cdab84fea3822c80aae5969ccb7b2
saagarp <saagarp@emergingbrilliance.org>
2013-03-25 10:19:03 -0700

    xcode

---
+++ DistribDB/DistribDB/DistribDB.h
@@ -0,0 +1,23 @@
+/*
+ *  DistribDB.h
+ *  DistribDB
+ *
+ *  Created by Saagar Patel on 3/25/13.
+ *  Copyright (c) 2013 Saagar Patel. All rights reserved.
+ *
+ */
+
+#ifndef DistribDB_
+#define DistribDB_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class DistribDB
+{
+	public:
+		void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ DistribDB/DistribDB/DistribDBPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  DistribDBPriv.h
+ *  DistribDB
+ *
+ *  Created by Saagar Patel on 3/25/13.
+ *  Copyright (c) 2013 Saagar Patel. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class DistribDBPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
